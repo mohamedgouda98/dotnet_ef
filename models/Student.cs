f7f913306f6ccca1300b0ff089ce8ac26f9b2aec
Mohamed Gouda <mohamedgouda@Mohameds-MacBook-Pro.local>
2021-03-17 14:53:17 +0200

    database bug

---
+++ models/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace project1.models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ models/testDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace project1.models
+{
+    public class testDbContext : DbContext
+    {
+        private readonly IConfiguration _config;
+
+        public testDbContext(DbContextOptions options, IConfiguration config) : base(options)
+        {
+            _config = config;
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_config.GetConnectionString("CodeCamp"));
+
+        }
+
+    }
+}
